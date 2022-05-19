60df4061012e358ae7fb7660e6f3e88610b0bccb
ahmed galal <ahmedgabdelaziz4@gmail.com>
2022-05-19 02:35:35 +0200

    first challeng

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hellooo")
+var name = 'Ahmed'
+var age = '40'
+
+var output = `I am ${name} and I am ${age} years old .. I am  ${age > 30? "too old":"still young"}`
+console.log(output)
